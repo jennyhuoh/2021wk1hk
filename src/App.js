7218809d9784dd37bd564390c891c74da11dd43a
jennyhuoh <huoh.li@gmail.com>
2021-02-23 21:49:07 +0800

    first upload

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="big">
+        <div className="img">
+            <img src="" alt="" />
+        </div>
+      </div>
+      <div className = "area1">
+        <div className = "myinfo">
+            <div>
+                <img className = "idphoto" src = "images/img_idphoto.jpg"/>
+            </div>
+            <div className = "myname">
+                姓名：霍佳立<br />信箱：s110619040@stu.ntue.edu.tw<br />學校：國立臺北教育大學<br />系所：數位科技設計學系
+            </div>
+            <div>
+                <div className = "mypro">技術<br /> 網頁/程式語言：HTML/CSS/JS/React-native/C++<br /> 美術設計：AdobeXD/Adobe Photoshop<br /> 影片剪輯：Adobe After Effect/iMovie</div>
+                <div className = "myint">興趣：料理</div>
+            </div>
+        </div>
+        <div className = "a1down">
+            <div className = "myexp">
+                經歷：<br />
+                大型電腦公司 資料標註工讀生<br />
+                包裝設計公司 設計助理<br />
+                國立臺北教育大學 芝山親善大使10th隊長<br />
+                國立臺北教育大學 數位營 生活長
+            </div>
+            <div className = "mylif">
+                <div>
+                    <div className = "photopic">生活照：</div>
+                    <div className="photo photo1"><img src="images/img1.png" alt="" /></div>
+		              <div className="photo photo2"><img src="images/img2.png" alt="" /></div>
+		              <div className="photo photo3"><img src="images/img3.png" alt="" /></div>
+		              <div className="photo photo4"><img src="images/img4.png" alt="" /></div>
+		              <div className="photo photo5"><img src="images/img5.png" alt="" /></div>
+		              <div className="photo photo6"><img src="images/img6.png" alt="" /></div>
+                </div>
+            </div>
+        </div>
+        <div className = "gopro">
+            <a className = "goprotext" href = "#area2">前往作品集 go</a>
+        </div>
+        
+    </div>
+    <div className = "area2" id = "area2">
+        <div className = "a2topic"><text className = "a2topicword">網站設計與賞析課堂作品</text></div>
+        <div className = "products">
+            <div className = "1">
+                <div className = "w"><text className = "ww">coffee menu</text></div>
+                <div className = "circle"><a className = "circlew" href = "http://jennyhuoh.epizy.com/coffee/">Click</a><a className = "circlew2" href = "http://jennyhuoh.epizy.com/coffee/">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">typography</text></div>
+                <div className = "circle"><a className = "circlew" href = "https://codepen.io/jennyhuoh/full/wvaXNQZ">Click</a><a className = "circlew2" href = "https://codepen.io/jennyhuoh/full/wvaXNQZ">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">six colors</text></div>
+                <div className = "circle"><a className = "circlew" href = "https://codepen.io/jennyhuoh/full/NWGWPwN">Click</a><a className = "circlew2" href = "https://codepen.io/jennyhuoh/full/NWGWPwN">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">pic n words</text></div>
+                <div className = "circle"><a className = "circlew" href = "https://codepen.io/jennyhuoh/full/BaojVLb">Click</a><a className = "circlew2" href = "https://codepen.io/jennyhuoh/full/BaojVLb">Go!</a></div>
+            </div>
+            <div clasNameclassName = "1">
+                <div className = "w"><text className = "ww">one page</text></div>
+                <div className = "circle"><a className = "circlew" href = "https://codepen.io/jennyhuoh/full/bGVEKQN">Click</a><a className = "circlew2" href = "https://codepen.io/jennyhuoh/full/bGVEKQN">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">mid hw</text></div>
+                <div className = "circle"><a className = "circlew" href = "https://codepen.io/jennyhuoh/full/vYNXeZm">Click</a><a className = "circlew2" href = "https://codepen.io/jennyhuoh/full/vYNXeZm">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">to the top</text></div>
+                <div className = "circle"><a className = "circlew" href = "https://codepen.io/jennyhuoh/pen/xxwLWNv">Click</a><a className = "circlew2" href = "https://codepen.io/jennyhuoh/pen/xxwLWNv">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">ol/ul li</text></div>
+                <div className = "circle"><a className = "circlew" href = "https://codepen.io/jennyhuoh/full/JjYyvMX">Click</a><a className = "circlew2" href = "https://codepen.io/jennyhuoh/full/JjYyvMX">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">21天生活小改變</text></div>
+                <div className = "circle"><a className = "circlew" href = "https://codepen.io/jennyhuoh/full/Baodxbq">Click</a><a className = "circlew2" href = "https://codepen.io/jennyhuoh/full/Baodxbq">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">導覽列改寫</text></div>
+                <div className = "circle"><a className = "circlew" href = "https://codepen.io/jennyhuoh/full/YzyeLWB">Click</a><a className = "circlew2" href = "https://codepen.io/jennyhuoh/full/YzyeLWB">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">RWD</text></div>
+                <div className = "circle"><a className = "circlew" href = "http://jennyhuoh.epizy.com/0514_web2/">Click</a><a className = "circlew2" href = "http://jennyhuoh.epizy.com/0514_web2/">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">monkey</text></div>
+                <div className = "circle"><a className = "circlew" href = "http://jennyhuoh.epizy.com/0528_monkey/">Click</a><a className = "circlew2" href = "http://jennyhuoh.epizy.com/0528_monkey/">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">light control</text></div>
+                <div className = "circle"><a className = "circlew" href = "http://jennyhuoh.epizy.com/L13/">Click</a><a className = "circlew2" href = "http://jennyhuoh.epizy.com/L13/">Go!</a></div>
+            </div>
+            <div className = "1">
+                <div className = "w"><text className = "ww">photos</text></div>
+                <div className = "circle"><a className = "circlew" href = "http://jennyhuoh.epizy.com/L14/">Click</a><a className = "circlew2" href = "http://jennyhuoh.epizy.com/L14/">Go!</a></div>
+            </div>
+
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
